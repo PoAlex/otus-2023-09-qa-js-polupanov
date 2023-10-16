3640940c24ec940faa9f183ae1d47b6f82596a20
Alexey Polupanov <polupanov.a.v@yandex.ru>
2023-10-16 22:21:08 +1000

    fix tale.js //add return in func

--- homework3/tale.js
+++ homework3/tale.js
@@ -1,20 +1,24 @@
 function kolobok(namePerson1) {
-    switch (namePerson) {
-        case 'дедушка':
-            console.log("Я от дедушки ушел")
-            break;
-            case 'заяц':
-                console.log("Я от зайца ушел")
-                break;
-                case 'лиса':
-                    console.log("Меня съели")
-                    break;
-        default:
-            break;
-    }
+  switch (namePerson1) {
+    case "дедушка":
+      console.log("Я от дедушки ушел");
+      break;
+    case "заяц":
+      console.log("Я от зайца ушел");
+      break;
+    case "лиса":
+      console.log("Меня съели");
+      break;
+    default:
+      break;
+  }
 }
 
 function newYear(namePerson2) {
-    console.log(`${namePerson2}! ${namePerson2}! ${namePerson2}!`)
+  return `${namePerson2}! ${namePerson2}! ${namePerson2}!`;
 }
 
+kolobok("дедушка");
+
+const message = newYear("Снегурочка");
+console.log(message);
