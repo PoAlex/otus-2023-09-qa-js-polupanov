354d781b19d7d6bcdf6f20986608f7d5bb6576d7
PoAlex <polupanov.a.v@gmail.com>
2024-01-05 01:11:06 +1000

    wip

--- tests/hommework18/test.spec.js
+++ tests/hommework18/test.spec.js
@@ -7,7 +7,7 @@ test.afterEach(async ({ page }, testInfo) => {
 });
 
 test('Авторизация', async ({ page }) => {
-  await test.step('Негативный тест', async () => {
+  await test.step('Негативный тест. Автроризация без ввода данных', async () => {
     await page.goto('https://try.vikunja.io/login');
     // await page.getByRole('button', { name: 'Login' }).click();
     await page.locator('//button[normalize-space()=\'Login\']').click();
@@ -15,15 +15,23 @@ test('Авторизация', async ({ page }) => {
     await expect(page.getByTestId('loginform')).toContainText('Please provide a username.');
     await expect(page.getByTestId('loginform')).toContainText('Please provide a password.');
   }, { box: true });
+
+  await test.step('Негативный тест. Неверный логин или пароль', async () => {
+    await page.getByTestId('username').fill('Alex_Po');
+    await page.getByTestId('password').fill('Test_123');
+    await page.locator('[tabindex=\'4\'][type=\'button\']').click();
+
+    await expect(page.getByTestId('app')).toContainText('Wrong username or password.');
+  }, { box: true });
+
   await test.step('Actions', async () => {
     // await page.goto('https://try.vikunja.io/login');
-    await page.getByTestId('username').click();
     await page.getByTestId('username').fill('Alex_Po');
-    await page.getByTestId('username').press('Tab');
     await page.getByTestId('password').fill('Test_12345');
     // await page.getByRole('button', { name: 'Login' }).click();
     await page.locator('[tabindex=\'4\'][type=\'button\']').click();
   }, { box: true });
+
   await test.step('Check', async () => {
     await expect(page.getByRole('heading', { name: 'Доброй ночи, Alex_Po!' })).toBeVisible();
   }, { box: true });
