a9c82dc9ef2db69f5a29f12d843a241f85643002
PoAlex <polupanov.a.v@gmail.com>
2024-01-05 00:58:13 +1000

    add box

--- tests/hommework18/test.spec.js
+++ tests/hommework18/test.spec.js
@@ -1,6 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-// const page = await browser.newPage();
+test.afterEach(async ({ page }, testInfo) => {
+  console.log(`Finished ${testInfo.title} with status ${testInfo.status}`);
+
+  if (testInfo.status !== testInfo.expectedStatus) { console.log(`Did not run as expected, ended up at ${page.url()}`); }
+});
 
 test('Авторизация', async ({ page }) => {
   await test.step('Негативный тест', async () => {
@@ -10,7 +14,7 @@ test('Авторизация', async ({ page }) => {
 
     await expect(page.getByTestId('loginform')).toContainText('Please provide a username.');
     await expect(page.getByTestId('loginform')).toContainText('Please provide a password.');
-  });
+  }, { box: true });
   await test.step('Actions', async () => {
     // await page.goto('https://try.vikunja.io/login');
     await page.getByTestId('username').click();
@@ -19,10 +23,10 @@ test('Авторизация', async ({ page }) => {
     await page.getByTestId('password').fill('Test_12345');
     // await page.getByRole('button', { name: 'Login' }).click();
     await page.locator('[tabindex=\'4\'][type=\'button\']').click();
-  });
+  }, { box: true });
   await test.step('Check', async () => {
     await expect(page.getByRole('heading', { name: 'Доброй ночи, Alex_Po!' })).toBeVisible();
-  });
+  }, { box: true });
 });
 
 test('Проверка главного меню', async ({ page }) => {
